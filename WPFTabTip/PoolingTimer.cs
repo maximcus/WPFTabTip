6f2b9e2f746245dfa10b419d1639ed5e5a303977
m.fedotov <m.fedotov@LC-OLAPWS8>
2016-08-10 17:09:13 +0300

    Первичная реализация класса TabTip

---
+++ WPFTabTip/PoolingTimer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPFTabTip
+{
+    public static class PoolingTimer
+    {
+        private static Timer timer;
+
+        public static void Start(Func<bool> StopTimer, TimeSpan dueTime, TimeSpan period)
+        {
+            if (timer == null)
+            {
+                timer = new Timer(
+                    (obj) =>
+                    {
+                        if (StopTimer())
+                            Dispose();
+                    }, 
+                state: null, 
+                dueTime: (int) dueTime.TotalMilliseconds, 
+                period: (int) period.TotalMilliseconds);
+            }
+        }
+
+        private static void Dispose()
+        {
+            try
+            {
+                timer.Dispose();
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+            finally
+            {
+                timer = null;
+            }
+        }
+    }
+}
---
+++ WPFTabTip/TabTip.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace WPFTabTip
+{
+    public static class TabTip
+    {
+        private const string tabTipWindowClassName = "IPTip_Main_Window";
+        private const string TabTipExecPath = @"C:\Program Files\Common Files\microsoft shared\ink\TabTip.exe";
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool PostMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(String sClassName, String sAppName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(HandleRef hWnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public readonly int Left;        // x position of upper-left corner
+            public readonly int Top;         // y position of upper-left corner
+            public readonly int Right;       // x position of lower-right corner
+            public readonly int Bottom;      // y position of lower-right corner
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern UInt32 GetWindowLong(IntPtr hWnd, int nIndex);
+
+        /// <summary>
+        /// Specifies we wish to retrieve window styles.
+        /// </summary>
+        private const int GWL_STYLE = -16;
+
+        private const uint KeyboardClosedStyle = 2617245696;
+
+        /// <summary>
+        /// Signals that TabTip was closed
+        /// </summary>
+        public static event Action Closed;
+
+        private static IntPtr GetTabTipWindowHandle() => FindWindow(tabTipWindowClassName, null);
+
+
+        /// <summary>
+        /// Open TabTip
+        /// </summary>
+        public static void Open()
+        {
+            Process.Start(TabTipExecPath);
+            StartTimerForTabTipClosedEvent();
+        }
+
+        /// <summary>
+        /// Close TabTip
+        /// </summary>
+        public static void Close()
+        {
+            const uint WM_SYSCOMMAND = 274;
+            const uint SC_CLOSE = 61536;
+            PostMessage(GetTabTipWindowHandle().ToInt32(), WM_SYSCOMMAND, (int)SC_CLOSE, 0);
+        }
+
+        private static void StartTimerForTabTipClosedEvent()
+        {
+            PoolingTimer.Start(() =>
+            {
+                IntPtr KeyboardWnd = GetTabTipWindowHandle();
+                if (KeyboardWnd.ToInt32() == 0 || GetWindowLong(KeyboardWnd, GWL_STYLE) == KeyboardClosedStyle)
+                {
+                    Closed?.Invoke();
+                    return true;
+                }
+
+                return false;
+            }, 
+            dueTime: TimeSpan.FromMilliseconds(700), 
+            period: TimeSpan.FromMilliseconds(50));
+        }
+
+        [SuppressMessage("ReSharper", "ConvertIfStatementToReturnStatement")]
+        public static Rectangle GetTabTipRectangle()
+        {
+            RECT rect;
+            
+            if (!GetWindowRect(new HandleRef(null, GetTabTipWindowHandle()), out rect))
+                return new Rectangle();
+
+            return new Rectangle(x: rect.Left, y: rect.Top, width: rect.Right - rect.Left + 1, height: rect.Bottom - rect.Top + 1);
+        }
+    }
+}
