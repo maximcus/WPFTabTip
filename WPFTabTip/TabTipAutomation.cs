6dcdd6a8fae94889fd98d317ebbee25ef4356281
Wouter <w.l.devries@gmail.com>
2017-01-17 12:03:38 +0100

    Add support for ignoring multiple keyboard devices (#8)
    * Add support for ignoring multiple devices and properly dispose WMI objects
    
    * Properly dispose WMI query
    
    * Reinstate the ListOfHardwareKeyboardsToIgnoreIfSingleInstance property

--- WPFTabTip/TabTipAutomation.cs
+++ WPFTabTip/TabTipAutomation.cs
@@ -37,17 +37,23 @@ namespace WPFTabTip
             get { return HardwareKeyboard.IgnoreOptions; }
             set { HardwareKeyboard.IgnoreOptions = value; } 
         }
-
+      
         /// <summary>
         /// Subscribe to this event if you want to know about exceptions (errors) in this library
         /// </summary>
         public static event Action<Exception> ExceptionCatched;
-
+      
         /// <summary>
         /// Description of keyboards to ignore if there is only one instance of given keyboard.
         /// If you want to ignore some ghost keyboard, add it's description to this list
         /// </summary>
-        public static List<string> ListOfHardwareKeyboardsToIgnoreIfSingleInstance => HardwareKeyboard.IgnoreIfSingleInstance;
+        public static List<string> ListOfHardwareKeyboardsToIgnoreIfSingleInstance => HardwareKeyboard.IgnoredDevices;
+
+        /// <summary>
+        /// Description of keyboards to ignore.
+        /// If you want to ignore some ghost keyboard, add it's description to this list
+        /// </summary>
+        public static List<string> IgnoredDevices => HardwareKeyboard.IgnoredDevices;
 
         private static void AutomateTabTipClose(IObservable<Tuple<UIElement, bool>> focusObservable, Subject<bool> tabTipClosedSubject)
         {
